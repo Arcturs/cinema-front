2711fb3d6e8c7dedcf55c99aa2543522adb7a051
Arcturs <yaninastya2010@yandex.ru>
2022-10-07 20:02:20 +0300

    adding default API

---
+++ src/API/ScreenAPI.ts
@@ -0,0 +1,32 @@
+import axios from "axios";
+import userAPI from "./UserAPI";
+
+class ScreenAPI {
+
+    getAllScreens(onSuccess: any, onFail: any, params: {[key: string]: any}) {
+        axios.get(`${process.env.API_URL}/screen`, {
+            params: params
+        })
+            .then(onSuccess)
+            .catch(onFail);
+    }
+
+    getScreenInfo(onSuccess: any, onFail: any, screenId: any) {
+        axios.get(`${process.env.API_URL}/screen/${screenId}`)
+            .then(onSuccess)
+            .catch(onFail);
+    }
+
+    createScreen(onSuccess: any, onFail: any, data: {[key: string]: any}) {
+        axios.post(`${process.env.API_URL}/screen`, data, {
+            headers: {
+                Authorization: userAPI.authHeader()
+            }
+        })
+            .then(onSuccess)
+            .catch(onFail);
+    }
+}
+
+let screenAPI = new ScreenAPI();
+export default screenAPI;
